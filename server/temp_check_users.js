ae170c0ab665cdbecea1f54d906391c28a6c8eff
Ahmad Tellawy <ahmadtellawy@socialinsight.com>
2026-05-16 06:28:50 +0300

    Fix Vercel build by excluding server from root tsconfig

--- server/temp_check_users.js
+++
@@ -1,1 +0,0 @@
-const main = async () = const prisma = new (require('@prisma/client').PrismaClient)(); const users = await prisma.user.findMany(); console.log(JSON.stringify(users, null, 2)); }; main();  
